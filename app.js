3a3986737c3806d94128d02a8327a16d155990b5
nithinjadi <143948938+nithinjadi@users.noreply.github.com>
2024-11-07 00:29:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let k=document.getElementById("button");
+let m=document.getElementById("para");
+
+function a(){
+    m.style.backgroundColor="red"
+    m.style.fontSize="60px"
+}
+
+let k1=document.getElementById("btn1");
+let m1=document.getElementById("div");
+function b(){
+    m1.innerHTML=`<img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg" width="200" style="border-radius:30px";>
+                  <img src="https://th.bing.com/th/id/OIG1.wQ7nqzXG6LLji1s3MrOP" width="200" style="border-radius:100px;">
+                  <img src="https://th.bing.com/th/id/OIG2.9O4YqGf98tiYzjKDvg7L" width="200" style="border-radius:50%";>`
+}
